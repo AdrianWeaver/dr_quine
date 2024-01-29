81994681a96c7c77174b2d9c51d8b69ca22fd648
aweaver <aweaver@student.42.fr>
2024-01-29 17:01:03 +0100

    WIP GRACE:
    Simple layout for C second exercise.

---
+++ Grace/C/Grace.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#define X "#include <stdio.h>%c#define obfuscated int m##a##i##n()%c#define u char *str=%c%s%c%cobfuscated{//this comment complies with the subject%c"
+#define C int f=open("Grace.c", O_WRONLY);dprintf(f,X,10,10,32,X,32,10,10);close(f);
+#define O int m##a##i##n(){C}
+O//this comment complies with the subject
