42c39f7fcf7f228325b95a92161c3643f5bce989
aweaver <aweaver@student.42.fr>
2024-01-29 17:39:16 +0100

    WIP GRACE WITH SECOND OUTPUT:
    Because the idea behind this code is to rewrite the source.
    I temporarily set the output to a different file in order to not
    modify it by mistake :)
    
    This file is grace_output.
    The temporary make diff rule compare Grace.c and Grace_output.c

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -2,6 +2,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 #define X "#include <stdio.h>%c#define obfuscated int m##a##i##n()%c#define u char *str=%c%s%c%cobfuscated{//this comment complies with the subject%c"
-#define C int f=open("Grace.c", O_WRONLY);dprintf(f,X,10,10,32,X,32,10,10);close(f);
+#define C int f=open("Grace_output.c", O_CREAT | O_WRONLY, 0640);dprintf(f,X,10,10,32,X,32,10,10);close(f);
 #define O int m##a##i##n(){C}
 O//this comment complies with the subject
