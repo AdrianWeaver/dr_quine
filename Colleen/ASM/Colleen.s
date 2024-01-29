4bbcc708f1ad75d0395b2ef285a167a37a6af0f1
Adrian WEAVER <aweaver@bess-f4r1s14.clusters.42paris.fr>
2024-01-29 08:47:24 +0100

    UPDATE COLLEEN.S COMPLYING WITH SUBJECT:
    Update colleen.s to comply with subject.
    
    Adding comments and call to function

--- Colleen/ASM/Colleen.s
+++ Colleen/ASM/Colleen.s
@@ -1,25 +1,27 @@
-section .data
-s db "section .data%cs db %c%s%c%csection .text%cextern printf,exit%cglobal main%cmain:%cmov rdi,s%cmov rsi,10%cmov rdx,34%cmov r8,34%cmov r9,10%cmov rcx,24%cpush rbp%cws:push 10%cloop ws%cmov rcx,s%cxor rax,rax%ccall printf%cmov rcx,24%csa:pop rax%cloop sa%cpop rbp%cmov rdi,0%ccall exit%csection .bss"
+section .data;comment one
+s db "section .data;comment one%cs db %c%s%c%csection .text%cextern printf,exit%cglobal main%cmain:;comment two%cmov rdi,s%cmov rsi,10%cmov rdx,34%cmov r8,34%cmov r9,10%cmov rcx,26%cpush rbp%cws:push 10%cloop ws%cmov rcx,s%cxor rax,rax%ccall printf%cmov rcx,26%csa:pop rax%cloop sa%cpop rbp%cmov rdi,0%cjmp finish%cfinish:%ccall exit%csection .bss"
 section .text
 extern printf,exit
 global main
-main:
+main:;comment two
 mov rdi,s
 mov rsi,10
 mov rdx,34
 mov r8,34
 mov r9,10
-mov rcx,24
+mov rcx,26
 push rbp
 ws:push 10
 loop ws
 mov rcx,s
 xor rax,rax
 call printf
-mov rcx,24
+mov rcx,26
 sa:pop rax
 loop sa
 pop rbp
 mov rdi,0
+jmp finish
+finish:
 call exit
 section .bss
