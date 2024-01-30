0d2d154a0ee3acda21ea5d390342ddf21b051d37
aweaver <aweaver@student.42.fr>
2024-01-30 16:20:29 +0100

    ADD WORKING SOLUTION GRACE IN C:
    This one is a working solution for grace.

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,7 +1,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
-#define X "#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c#define X %c%s%c%c#define C int f=open(%cGrace_output.c%c, O_CREAT | O_WRONLY, 0640);dprintf(f,X,10,10,10,34,X,34,10,34,34,10,10);close(f);%c#define O int m##a##i##n(){C}%cO//this comment complies with the subject"
-#define C int f=open("Grace_output.c", O_CREAT | O_WRONLY, 0640);dprintf(f,X,10,10,10,34,X,34,10,34,34,10,10);close(f);
+#define X "#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c#define X %c%s%c%c#define C int f=open(%cGrace.c%c, O_CREAT | O_WRONLY, 0640);dprintf(f,X,10,10,10,34,X,34,10,34,34,10,10);close(f);%c#define O int m##a##i##n(){C}%cO//this comment complies with the subject"
+#define C int f=open("Grace.c", O_CREAT | O_WRONLY, 0640);dprintf(f,X,10,10,10,34,X,34,10,34,34,10,10);close(f);
 #define O int m##a##i##n(){C}
 O//this comment complies with the subject
