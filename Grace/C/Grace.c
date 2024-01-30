c207ae61ae5339a8c11c84424099add9c4934bc1
aweaver <aweaver@student.42.fr>
2024-01-30 16:19:31 +0100

    ADD PARTIAL SOLUTION GRACE IN C:
    Grace now works but with a different output, so it's easier to test.

--- Grace/C/Grace.c
+++ Grace/C/Grace.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define X "#include <stdio.h>%c#define obfuscated int m##a##i##n()%c#define u char *str=%c%s%c%cobfuscated{//this comment complies with the subject%c"
-#define C int f=open("Grace_output.c", O_CREAT | O_WRONLY, 0640);dprintf(f,X,10,10,32,X,32,10,10);close(f);
+#include <stdio.h>
+#define X "#include <fcntl.h>%c#include <unistd.h>%c#include <stdio.h>%c#define X %c%s%c%c#define C int f=open(%cGrace_output.c%c, O_CREAT | O_WRONLY, 0640);dprintf(f,X,10,10,10,34,X,34,10,34,34,10,10);close(f);%c#define O int m##a##i##n(){C}%cO//this comment complies with the subject"
+#define C int f=open("Grace_output.c", O_CREAT | O_WRONLY, 0640);dprintf(f,X,10,10,10,34,X,34,10,34,34,10,10);close(f);
 #define O int m##a##i##n(){C}
 O//this comment complies with the subject
