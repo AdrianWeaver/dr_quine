47dc544cd782c489cd75c863c2fbb6655f052670
aweaver <aweaver@student.42.fr>
2024-01-31 21:14:41 +0100

    WIP OPEN AND WRITE IN ASM:
    This is a sandbox commit in grace.s
    I went back to the roots to make sure I was able to open a file and
    write in it in asm as this is the first time I have to do that.
    
    Working my way up to complete the task. Writing readable code before
    obfuscation

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -1,29 +1,59 @@
 section .data		;mandatory comment
-s db "section .data		;mandatory comment%cs db %c%s%c%csection .text%cextern printf%c%macro magic,0%cmov rdi,s%cmov rsi,10%cmov rdx,34%cmov r8,s%cmov r9,34%cmov rcx,10		;last newline after string%cpush rbp		;here%cws:push 10%cloop ws%cmov rcx,s%cxor rax,rax%ccall printf%cmov rcx,26%csa:pop rax%cloop sa%cpop rbp%cmov rdi,0%ccall exit%c%endmacro%c%cglobal obfuscated%cobfuscated:%c	magic"
+file db "Grace_asm_output.txt", 0
+len: equ $-file
+error_msg db "There was an error in open", 10, 0
+error_len: equ $-error_msg
 section .text
 extern printf
 extern exit
-%macro magic 0
-mov rdi,s
-mov rsi,10
-mov rdx,34
-mov r8,s
-mov r9,34
-mov rcx,26
-push rbp
-ws:push 10
-loop ws
-mov rcx,s
-xor rax,rax
-call printf
-mov rcx,26
-sa:pop rax
-loop sa
-pop rbp
-mov rdi,0
-call exit
+global main
+
+%macro replace_open 0
+mov	rax,2			;this is the syscall for open
+mov rdi,file		;this is the file to be opened
+;be aware flags and mods are octal values stored on an int
+;next we need the flag it is stored on an 8 byte flag value
+	;O_WRONLY defined as 1o
+	;O_CREATE defined as 100o
+	;we need both so... 101o
+mov rsi, 0101o
+;because we are using open we also need the mode arg.
+	;these are the same as octal values in chmod so we want 640 here
+mov rdx, 0640o
+syscall
 %endmacro
 
-global obfuscated
-obfuscated:
-	magic
+main:
+	replace_open	;open file and stores fd in rax
+	mov rcx, rax	;saving fd
+	cmp rax, 0		;checking return of open
+	js error
+	mov rdi, rax	;prepare a write with accurate fd
+	mov rax, 1		;prepare a write syscall
+	mov rsi, file	;writing from address "file"
+	mov rdx, len	;for len characters
+	syscall			;calling write
+
+	
+	mov rdi, rcx	;preparing to close fd
+	mov rax, 3		;preparing close syscall
+	syscall			;calling close
+
+finish:
+	mov rax, 60		;preparing to call exit
+	mov rdi, 0		;return code 0
+	syscall			;calling exit
+
+
+error:
+	mov rax, 1		;preparing a write error
+	mov	rdi, 2		;writing on stderr
+	mov rsi, error_msg	;writing error
+	mov rdx, error_len	;for len of message
+	syscall
+	jmp finish
+
+;trick to replace the main
+;global obfuscated
+;obfuscated:
+;	magic
