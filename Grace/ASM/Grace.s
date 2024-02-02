e9c322815c133ea158ecdf6aa2479a6a9e4ddac1
aweaver <aweaver@student.42.fr>
2024-02-02 18:39:56 +0100

    WIP SEGFAULT (GRACE.S):
    Working on a segfault in strlen of dprintf...

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -1,6 +1,6 @@
 section .rodata	;mandatory comment
 file db "Grace_asm_output.txt", 0
-X db "section .data	;mandatory comment%cfile db %cGrace_asm_output.txt%c, 0%cX db %c%s%c%csection .text%cextern dprintf%cextern exit%cglobal _start%c%c%macro code 0%cmov	rax,2			;this is the syscall for open%cmov rdi,file		;this is the file to be opened%c;be aware flags and mods are octal values stored on an int%c;next we need the flag it is stored on an 8 byte flag value%c	;O_WRONLY defined as 1o%c	;O_CREATE defined as 100o%c	;we need both so... 101o%cmov rsi, 0101o%c;because we are using open we also need the mode arg.%c	;these are the same as octal values in chmod so we want 640 here%cmov rdx, 0640o%csyscall%ccmp rax, 0		;checking return of open%cjs quit%cpush rbp%cpush rax%c;dprintf here%cmov rdi,rax%cmov rsi,X%cmov rdx,10%cmov rcx,34%cmov r8,X%cmov r9,34%c;adding \ns%cws:push 10%cloop ws%cmov rcx,s%cxor rax,rax%ccall dprintf wrt ..plt%cmov rcx,26%csa:pop rax%cloop sa%cpop rcx%c;close and end%cpop rdi%cmov rax, 3		;preparing close syscall%csyscall			;calling close%cpop rax%cpop rbp%cquit%c%endmacro%c%c%macro quit 0%c	mov rax, 60		;preparing to call exit%c	mov rdi, 0		;return code 0%c	syscall			;calling exit%c%endmacro%c%c_start:%c	code	;open file and stores fd in rax"
+s db "section .data	;mandatory comment%cfile db %cGrace_asm_output.txt%c, 0%cX db %c%s%c%csection .text%cextern dprintf%cextern exit%cglobal _start%c%c%macro code 0%cmov	rax,2			;this is the syscall for open%cmov rdi,file		;this is the file to be opened%c;be aware flags and mods are octal values stored on an int%c;next we need the flag it is stored on an 8 byte flag value%c	;O_WRONLY defined as 1o%c	;O_CREATE defined as 100o%c	;we need both so... 101o%cmov rsi, 0101o%c;because we are using open we also need the mode arg.%c	;these are the same as octal values in chmod so we want 640 here%cmov rdx, 0640o%csyscall%ccmp rax, 0		;checking return of open%cjs quit%cpush rbp%cpush rax%c;dprintf here%cmov rdi,rax%cmov rsi,X%cmov rdx,10%cmov rcx,34%cmov r8,X%cmov r9,34%c;adding \ns%cws:push 10%cloop ws%cmov rcx,s%cxor rax,rax%ccall dprintf wrt ..plt%cmov rcx,26%csa:pop rax%cloop sa%cpop rcx%c;close and end%cpop rdi%cmov rax, 3		;preparing close syscall%csyscall			;calling close%cpop rax%cpop rbp%cquit%c%endmacro%c%c%macro quit 0%c	mov rax, 60		;preparing to call exit%c	mov rdi, 0		;return code 0%c	syscall			;calling exit%c%endmacro%c%c_start:%c	code	;open file and stores fd in rax", 10, 0
 section .text
 extern dprintf
 extern exit
@@ -25,13 +25,13 @@ push rbp
 push rax
 ;dprintf here
 mov rdi,rax
-mov rsi,X
+mov rsi,s
 mov rdx,10
-mov rcx,34
+;mov rcx,34
 mov r8,34
 mov r9,10
 push 34
-push X
+push s
 push 34
 ;adding \ns
 mov rcx,56
@@ -40,23 +40,18 @@ loop ws
 mov rcx,34
 xor rax,rax
 call dprintf wrt ..plt
-mov rcx,5
+mov rcx,59
 sa:pop rax
 loop sa
-pop rcx
 ;close and end
 pop rdi
-mov rax, 3		;preparing close syscall
-syscall			;calling close
-pop rax
 pop rbp
-quit
-%endmacro
-
-%macro quit 0
-	mov rax, 60		;preparing to call exit
-	mov rdi, 0		;return code 0
-	syscall			;calling exit
+mov rax,3		;preparing close syscall
+syscall			;calling close
+quit:
+mov rax,60		;preparing to call exit
+mov rdi,0		;return code 0
+syscall			;calling exit
 %endmacro
 
 _start:
