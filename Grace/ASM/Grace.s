60448b695d0d78843bdfda21e84a9d47cfa3d753
aweaver <aweaver@student.42.fr>
2024-02-03 10:45:05 +0100

    ADD WORKING SOLUTION FOR GRACE.S:
    Fixing the huge stack mistake I made resulting in a working solution.
    This problem got me puzzled for a while, because of a huge mistake
    I made and the confusion resulting with the stack misunderstanding...
    I usually squash WIP commits, to hide all this, but the process often
    results in days spent without commits shown on GH...
    
    Here I am being transparent on my mistake, that's how you learn!

--- Grace/ASM/Grace.s
+++ Grace/ASM/Grace.s
@@ -1,58 +1,52 @@
 section .rodata	;mandatory comment
-file db "Grace_asm_output.txt", 0
-s db "section .data	;mandatory comment%cfile db %cGrace_asm_output.txt%c, 0%cX db %c%s%c%csection .text%cextern dprintf%cextern exit%cglobal _start%c%c%macro code 0%cmov	rax,2			;this is the syscall for open%cmov rdi,file		;this is the file to be opened%c;be aware flags and mods are octal values stored on an int%c;next we need the flag it is stored on an 8 byte flag value%c	;O_WRONLY defined as 1o%c	;O_CREATE defined as 100o%c	;we need both so... 101o%cmov rsi, 0101o%c;because we are using open we also need the mode arg.%c	;these are the same as octal values in chmod so we want 640 here%cmov rdx, 0640o%csyscall%ccmp rax, 0		;checking return of open%cjs quit%cpush rbp%cpush rax%c;dprintf here%cmov rdi,rax%cmov rsi,X%cmov rdx,10%cmov rcx,34%cmov r8,X%cmov r9,34%c;adding \ns%cws:push 10%cloop ws%cmov rcx,s%cxor rax,rax%ccall dprintf wrt ..plt%cmov rcx,26%csa:pop rax%cloop sa%cpop rcx%c;close and end%cpop rdi%cmov rax, 3		;preparing close syscall%csyscall			;calling close%cpop rax%cpop rbp%cquit%c%endmacro%c%c%macro quit 0%c	mov rax, 60		;preparing to call exit%c	mov rdi, 0		;return code 0%c	syscall			;calling exit%c%endmacro%c%c_start:%c	code	;open file and stores fd in rax", 10, 0
+file db "Grace.s", 0
+s db "section .rodata	;mandatory comment%cfile db %cGrace.s%c, 0%cs db %c%s%c,0%c%csection .bss%csection .text%cextern dprintf%cextern exit%cglobal _start%c%c%%macro code 0%cmov	rax,2%cmov rdi,file%cmov rsi,01101o%cmov rdx,0640o%csyscall%ccmp rax,0%cjs quit%cpush rbx%cpush rbp%cmov rbp,rsp%cmov rbx,rax%cmov rdi,rax%cmov rsi,s%cmov rdx,10%cmov r8,34%cmov r9,10%cmov rcx,49%cws:push 10%cloop ws%cpush 34%cpush s%cpush 34%cmov rcx,34%cxor rax,rax%ccall dprintf wrt ..plt%cmov rcx,52%csa:pop rax%cloop sa%cpop rbp%cpop rbx%cmov rax,3%cmov	rdi,rbx%csyscall%cquit:%cmov rax,60%cmov rdi,0%csyscall%c%%endmacro%c%c_start:%c	code",0
+
+section .bss
 section .text
 extern dprintf
 extern exit
 global _start
 
 %macro code 0
-mov	rax,2			;this is the syscall for open
-mov rdi,file		;this is the file to be opened
-;be aware flags and mods are octal values stored on an int
-;next we need the flag it is stored on an 8 byte flag value
-	;O_WRONLY defined as 1o
-	;O_CREATE defined as 100o
-	;we need both so... 101o
-mov rsi, 0101o
-;because we are using open we also need the mode arg.
-	;these are the same as octal values in chmod so we want 640 here
-mov rdx, 0640o
+mov	rax,2
+mov rdi,file
+mov rsi,01101o
+mov rdx,0640o
 syscall
-cmp rax, 0		;checking return of open
+cmp rax,0
 js quit
+push rbx
 push rbp
-push rax
-;dprintf here
+mov rbp,rsp
+mov rbx,rax
 mov rdi,rax
 mov rsi,s
 mov rdx,10
-;mov rcx,34
 mov r8,34
 mov r9,10
+mov rcx,49
+ws:push 10
+loop ws
 push 34
 push s
 push 34
-;adding \ns
-mov rcx,56
-ws:push 10
-loop ws
 mov rcx,34
 xor rax,rax
 call dprintf wrt ..plt
-mov rcx,59
+mov rcx,52
 sa:pop rax
 loop sa
-;close and end
-pop rdi
 pop rbp
-mov rax,3		;preparing close syscall
-syscall			;calling close
+pop rbx
+mov rax,3
+mov	rdi,rbx
+syscall
 quit:
-mov rax,60		;preparing to call exit
-mov rdi,0		;return code 0
-syscall			;calling exit
+mov rax,60
+mov rdi,0
+syscall
 %endmacro
 
 _start:
-	code	;open file and stores fd in rax
+	code
