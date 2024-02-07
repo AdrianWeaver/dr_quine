67f6611390218a8331b855e54cc54b7c975d6216
aweaver <aweaver@student.42.fr>
2024-02-07 21:18:13 +0100

    WIP INCORRECT STRING FOR DPRINTF:
    Fixed a lot of small errors.
    Now need to work on this buffered quine sequence.

--- Sully/ASM/Sully.s
+++ Sully/ASM/Sully.s
@@ -11,7 +11,7 @@ arg7 db "-o",0
 arg8 db "./Sully",0
 argptr dq cc,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,0
 exeptr dq arg8,0
-s db "section .data%crecurs equ %d%ccc db %c/bin/nasm%c,0%carg1 db %cSully%d.s%c, 0%carg2 db %c-felf64%c,0%carg3 db %c;%c,0%carg4 db %c/bin/clang%c, 0%carg5 db %cSully%d.o%c, 0%carg6 db %c-lc%c,0%carg7 db %c-o%c,0%carg8 db %cSully%c,0%cargptr dq cc,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,0%cexeptr dq arg8%cs db %c%s%c%c%csection .text%cglobal main%cextern dprintf%cextern printf%cextern waitpid%cextern exit%c%c%%macro print 0%cpush rbp%cmov rbp,rsp%cmov rdi,r12%cmov rsi,s%cmov rdx,10%c;rcx is a 34%cmov r8,34%cmov r9,10%cmov rcx,49 ;change this value%cws:push 111%cloop ws%cpush 34%cpush s%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 10%cpush 10%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cmov rcx,34%ccall dprintf%cmov rcx,144%csa:pop rax%cloop sa%c%c%%endmacro%c%cmain:%c	push r12%c	push r13%c	mov r13, rdx%c	mov [arg1+5], byte '5'%c	mov [arg5+5], byte '5'%c	mov rax,2%c	mov rdi,arg1%c	mov rsi,01101o%c	mov rdx,0640o%c	syscall ;open%c	mov r12, rax%c	cmp rax,0%c	js quit%c	mov rax, 57%c	syscall ;fork%c	cmp rax, 0%c	js quit%c	cmp rax, 0%c	je child%c	sub rsp, 64%c	mov rdi, rax%c	mov rsi, rsp%c	mov rdx, 0%c	call waitpid ;waitpid%c	mov rcx, arg8%c	mov rsi, exeptr%c	mov rdi, rcx%c	mov rdx, r13%c	mov rax, 59%c	syscall 	;execve execution%c	add rsp, 64%c	mov rdi, r12%c	mov rax, 2%c	syscall ;close fd%cquit:%c	pop r13%c	pop r12%c	mov rdi,0%c	call exit%cchild:%c	print%c	mov rsi,cc %c	mov rdi,argptr%c	mov rdx,r13%c	mov rax,59%c	syscall ;execve compilation%c	mov rdi, r12%c	mov rax, 2%c	syscall ;close fd%c	jmp quit", 0
+s db "section .data%crecurs equ %d%ccc db %c/bin/nasm%c,0%carg1 db %cSully%d.s%c, 0%carg2 db %c-felf64%c,0%carg3 db %c;%c,0%carg4 db %c/bin/clang%c, 0%carg5 db %cSully%d.o%c, 0%carg6 db %c-lc%c,0%carg7 db %c-o%c,0%carg8 db %cSully%c,0%cargptr dq cc,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,0%cexeptr dq arg8%cs db %c%s%c%c%csection .text%cglobal main%cextern dprintf%cextern printf%cextern waitpid%cextern exit%c%c%%macro print 0%cpush rbp%cmov rbp,rsp%cmov rdi,r12%cmov rsi,s%cmov rdx,10%c;rcx is a 34%cmov r8,34%cmov r9,10%cmov rcx,111 ;change this value%cws:push 10%cloop ws%cpush 34%cpush s%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 10%cpush 10%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cmov rcx,34%ccall dprintf%cmov rcx,145%csa:pop rax%cloop sa%c%c%%endmacro%c%cmain:%c	xor rax,rax%c	push r12%c	push r13%c	mov r13, rdx%c	mov [arg1+5], byte '5'%c	mov [arg5+5], byte '5'%c	mov rax,2%c	mov rdi,arg1%c	mov rsi,01101o%c	mov rdx,0640o%c	syscall ;open%c%c	cmp rax,0%c	js quit%c	mov r12,rax%c	print%c	mov rdi, r12%c	mov rax,3%c	syscall%c	mov rax, 57%c	syscall ;fork%c	cmp rax, 0%c	js quit%c	cmp rax, 0%c	je child%c	sub rsp, 64%c	mov rdi, rax%c	mov rsi, rsp%c	mov rdx, 0%c	call waitpid%c	add rsp,64%c ;waitpid%c	mov rcx, arg8%c	mov rsi, exeptr%c	mov rdi, rcx%c	mov rdx, r13%c	mov rax, 59%c	syscall 	;execve execution%cquit:%c	pop r13%c	pop r12%c	mov rdi,0%c	call exit%cchild:%c	mov rsi,cc %c	mov rdi,argptr%c	mov rdx,r13%c	mov rax,59%c	syscall ;execve compilation%c	jmp quit", 0
 section .text
 global main
 extern dprintf
@@ -28,7 +28,7 @@ mov rdx,10
 ;rcx is a 34
 mov r8,10
 mov r9,34
-mov rcx,112 ;change this value
+mov rcx,111 ;change this value
 ws:push 10
 loop ws
 push 34		;send
@@ -102,12 +102,12 @@ main:
 	js quit
 	cmp rax, 0
 	je child
-	add rsp, 64
+	sub rsp, 64
 	mov rdi, rax
 	mov rsi, rsp
 	mov rdx, 0
 	call waitpid ;waitpid
-	sub rsp, 64
+	add rsp, 64
 	mov rdi, arg8
 	mov rsi, exeptr
 	mov rdx, r13
@@ -126,7 +126,4 @@ child:
 	mov rdx,r13
 	mov rax,59
 	syscall ;execve compilation
-	mov rdi, r12
-	mov rax, 3
-	syscall ;close fd
 	jmp quit
