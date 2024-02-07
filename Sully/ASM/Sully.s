b0d75680acc89e56205ab8b7a94e3ab81552908b
aweaver <aweaver@student.42.fr>
2024-02-07 22:41:44 +0100

    UPDATE QUINE (SULLY):
    This is a working quine except something is wrong with the compilation
    
    It's apparently the only issue that needs to be solved and it should be
    a task completed.

--- Sully/ASM/Sully.s
+++ Sully/ASM/Sully.s
@@ -1,17 +1,17 @@
 section .data
 recurs equ 5
 cc db "/bin/nasm",0
-arg1 db "Sully5.s", 0
+arg1 db "Sully5.s",0
 arg2 db "-felf64",0
 arg3 db ";",0
-arg4 db "/bin/clang", 0
-arg5 db "Sully5.o", 0
+arg4 db "/bin/clang",0
+arg5 db "Sully5.o",0
 arg6 db "-lc",0
 arg7 db "-o",0
 arg8 db "./Sully",0
 argptr dq cc,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,0
 exeptr dq arg8,0
-s db "section .data%crecurs equ %d%ccc db %c/bin/nasm%c,0%carg1 db %cSully%d.s%c, 0%carg2 db %c-felf64%c,0%carg3 db %c;%c,0%carg4 db %c/bin/clang%c, 0%carg5 db %cSully%d.o%c, 0%carg6 db %c-lc%c,0%carg7 db %c-o%c,0%carg8 db %c./Sully%c,0%cargptr dq cc,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,0%cexeptr dq arg8,0%cs db %c%s%c%c%csection .text%cglobal main%cextern dprintf%cextern printf%cextern waitpid%cextern exit%c%c%%macro print 0%cpush rbp%cmov rbp,rsp%cmov rdi,r12%cmov rsi,s%cmov rdx,10%c;rcx is a 34%cmov r8,10%cmov r9,34%cmov rcx,111 ;change this value%cws:push 10%cloop ws%cpush 34%cpush s%cpush 34%cpush 10%push 10%cpush 10%cpush 10%ccpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush r14%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush r14%cpush 34%cpush 10%cpush 34%cmov rcx,r14%ccall dprintf%cmov rcx,145%csa:pop rax%cloop sa%c%c%%endmacro%c%cmain:%c	xor rax,rax%c	cmp rax,recurs%c	jz	quit%c	push r12%c	push r13%c	push r14%c	push	r15%c	mov r13, rdx%c	mov rax,2%c	mov rdi,arg1%c	mov rsi,01101o%c	mov rdx,0640o%c	syscall ;open%c	cmp rax,0%c	js quit%c	mov r12,rax%c	print%c	mov rdi, r12%c	mov rax,3%c	syscall%c	mov rax, 57%c	syscall ;fork%c	cmp rax, 0%c	js quit%c	cmp rax, 0%c	je child%c	sub rsp, 64%c	mov rdi, rax%c	mov rsi, rsp%c	mov rdx, 0%c	call waitpid%c	add rsp,64%c	mov rcx, arg8%c	mov rsi, exeptr%c	mov rdi, rcx%c	mov rdx, r13%c	mov rax, 59%c	syscall 	;execve execution%cquit:%c	pop r13%c	pop r12%c	mov rdi,0%c	call exit%cchild:%c	mov rdi,cc %c	mov rsi,argptr%c	mov rdx,r13%c	mov rax,59%c	syscall%c	jmp quit", 0
+s db "section .data%crecurs equ %d%ccc db %c/bin/nasm%c,0%carg1 db %cSully%d.s%c,0%carg2 db %c-felf64%c,0%carg3 db %c;%c,0%carg4 db %c/bin/clang%c,0%carg5 db %cSully%d.o%c,0%carg6 db %c-lc%c,0%carg7 db %c-o%c,0%carg8 db %c./Sully%c,0%cargptr dq cc,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,0%cexeptr dq arg8,0%cs db %c%s%c,0%c%csection .text%cglobal main%cextern dprintf%cextern printf%cextern waitpid%cextern exit%c%c%%macro print 0%cpush rbp%cmov rbp,rsp%cmov rdi,r12%cmov rsi,s%cmov rdx,10%c;rcx is a 34%cmov r8,10%cmov r9,34%cmov rcx,118 ;change this value%cws:push 10%cloop ws%cpush 34%cpush s%cpush 34%cpush 10%cpush 10%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush r14%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush r14%cpush 34%cpush 10%cpush 34%cmov rcx,r14%cxor rax,rax%ccall dprintf%cmov rcx,151%csa:pop rax%cloop sa%cpop rbp%c%%endmacro%c%cmain:%cxor rax,rax%ccmp rax,recurs%cjz	quit%cpush r12%cpush r13%cpush r14%cpush r15%cmov r14,recurs%csub r14,1%cmov r13,rdx%cmov rax,2%cmov rdi,arg1%cmov rsi,01101o%cmov rdx,0640o%csyscall ;open%ccmp rax,0%cjs quit%cmov r12,rax%cprint%cmov rdi,r12%cmov rax,3%csyscall%cmov rax,57%csyscall ;fork%ccmp rax,0%cjs quit%ccmp rax,0%cje child%csub rsp,64%cmov rdi,rax%cmov rsi,rsp%cmov rdx,0%ccall waitpid%cadd rsp,64%cmov rdi,arg8%cmov rsi,exeptr%cmov rdx,r13%cmov rax,59%csyscall 	;execve execution%cquit:%cpop r15%cpop r14%cpop r13%cpop r12%cmov rdi,0%ccall exit%cchild:%cmov rdi,cc%cmov rsi,argptr%cmov rdx,r13%cmov rax,59%csyscall%cjmp quit%c",0
 section .text
 global main
 extern dprintf
@@ -28,102 +28,102 @@ mov rdx,10
 ;rcx is a 34
 mov r8,10
 mov r9,34
-mov rcx,111 ;change this value
+mov rcx,118 ;change this value
 ws:push 10
 loop ws
-push 34		;send
+push 34
 push s
-push 34		;sbegin
-push 10		;exeptr
-push 10		;argptr
-push 10		;arg8
+push 34
+push 10
+push 10
+push 10
 push 34
 push 34
-push 10		;arg7
+push 10
 push 34
 push 34
-push 10		;arg6
+push 10
 push 34
 push 34
-push 10		;arg5
-push 34
-push r14
-push 34
-push 10		;arg4
-push 34
-push 34
-push 10		;arg3
-push 34
-push 34
-push 10		;arg2
-push 34
-push 34
-push 10		;arg1
+push 10
 push 34
 push r14
 push 34
-push 10		;cc
+push 10
+push 34
+push 34
+push 10
+push 34
+push 34
+push 10
+push 34
+push 34
+push 10
+push 34
+push r14
+push 34
+push 10
 push 34
 mov rcx,r14
 xor rax,rax
 call dprintf
-mov rcx,145
+mov rcx,151
 sa:pop rax
 loop sa
 pop rbp
 %endmacro
 
 main:
-	xor	rax,rax
-	cmp rax, recurs
-	jz	quit
-	push r12
-	push r13
-	push r14
-	push r15
-	mov r14, recurs
-	sub r14, 1
-	mov r13, rdx
-	mov rax,2
-	mov rdi,arg1
-	mov rsi,01101o
-	mov rdx,0640o
-	syscall ;open
-	cmp rax,0
-	js quit
-	mov r12, rax
-	print
-	mov rdi, r12
-	mov rax, 3
-	syscall ;close fd
-	mov rax, 57
-	syscall ;fork
-	cmp rax, 0
-	js quit
-	cmp rax, 0
-	je child
-	sub rsp, 64
-	mov rdi, rax
-	mov rsi, rsp
-	mov rdx, 0
-	call waitpid ;waitpid
-	add rsp, 64
-	mov rdi, arg8
-	mov rsi, exeptr
-	mov rdx, r13
-	mov rax, 59
-	syscall 	;execve execution
+xor rax,rax
+cmp rax,recurs
+jz	quit
+push r12
+push r13
+push r14
+push r15
+mov r14,recurs
+sub r14,1
+mov r13,rdx
+mov rax,2
+mov rdi,arg1
+mov rsi,01101o
+mov rdx,0640o
+syscall ;open
+cmp rax,0
+js quit
+mov r12,rax
+print
+mov rdi,r12
+mov rax,3
+syscall
+mov rax,57
+syscall ;fork
+cmp rax,0
+js quit
+cmp rax,0
+je child
+sub rsp,64
+mov rdi,rax
+mov rsi,rsp
+mov rdx,0
+call waitpid
+add rsp,64
+mov rdi,arg8
+mov rsi,exeptr
+mov rdx,r13
+mov rax,59
+syscall 	;execve execution
 quit:
-	pop r15
-	pop r14
-	pop r13
-	pop r12
-	mov rdi,0
-	call exit
+pop r15
+pop r14
+pop r13
+pop r12
+mov rdi,0
+call exit
 child:
-	mov rdi,cc
-	mov rsi,argptr 
-	mov rdx,r13
-	mov rax,59
-	syscall ;execve compilation
-	jmp quit
+mov rdi,cc
+mov rsi,argptr
+mov rdx,r13
+mov rax,59
+syscall
+jmp quit
