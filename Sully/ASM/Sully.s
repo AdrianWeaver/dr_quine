8237e19d08fc33e4fc7c8990ca46b7b61b6fd71a
aweaver <aweaver@student.42.fr>
2024-02-08 09:09:14 +0100

    WORKING SOLUTION FOR SULLY IN ASM:
    Finally.
    After dreaming the solution, it was easier to fix everything else.
    
    The main issue was a misconception (don't know where it came from)
    with execve. I somehow built everything just as if execve was a shell
    
    So indeed it could not work. The easiest work around with the existing
    code was to change the first parameter to make it actually use a shell.
    
    This is dirty but it works to comply with the subject and I don't want
    to waste too much time on this.

--- Sully/ASM/Sully.s
+++ Sully/ASM/Sully.s
@@ -1,17 +1,13 @@
 section .data
 recurs equ 5
-cc db "/bin/nasm",0
-arg1 db "Sully5.s",0
-arg2 db "-felf64",0
-arg3 db ";",0
-arg4 db "/bin/clang",0
-arg5 db "Sully5.o",0
-arg6 db "-lc",0
-arg7 db "-o",0
-arg8 db "./Sully",0
-argptr dq cc,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,0
-exeptr dq arg8,0
-s db "section .data%crecurs equ %d%ccc db %c/bin/nasm%c,0%carg1 db %cSully%d.s%c,0%carg2 db %c-felf64%c,0%carg3 db %c;%c,0%carg4 db %c/bin/clang%c,0%carg5 db %cSully%d.o%c,0%carg6 db %c-lc%c,0%carg7 db %c-o%c,0%carg8 db %c./Sully%c,0%cargptr dq cc,arg1,arg2,arg3,arg4,arg5,arg6,arg7,arg8,0%cexeptr dq arg8,0%cs db %c%s%c,0%c%csection .text%cglobal main%cextern dprintf%cextern printf%cextern waitpid%cextern exit%c%c%%macro print 0%cpush rbp%cmov rbp,rsp%cmov rdi,r12%cmov rsi,s%cmov rdx,10%c;rcx is a 34%cmov r8,10%cmov r9,34%cmov rcx,118 ;change this value%cws:push 10%cloop ws%cpush 34%cpush s%cpush 34%cpush 10%cpush 10%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush r14%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush r14%cpush 34%cpush 10%cpush 34%cmov rcx,r14%cxor rax,rax%ccall dprintf%cmov rcx,151%csa:pop rax%cloop sa%cpop rbp%c%%endmacro%c%cmain:%cxor rax,rax%ccmp rax,recurs%cjz	quit%cpush r12%cpush r13%cpush r14%cpush r15%cmov r14,recurs%csub r14,1%cmov r13,rdx%cmov rax,2%cmov rdi,arg1%cmov rsi,01101o%cmov rdx,0640o%csyscall ;open%ccmp rax,0%cjs quit%cmov r12,rax%cprint%cmov rdi,r12%cmov rax,3%csyscall%cmov rax,57%csyscall ;fork%ccmp rax,0%cjs quit%ccmp rax,0%cje child%csub rsp,64%cmov rdi,rax%cmov rsi,rsp%cmov rdx,0%ccall waitpid%cadd rsp,64%cmov rdi,arg8%cmov rsi,exeptr%cmov rdx,r13%cmov rax,59%csyscall 	;execve execution%cquit:%cpop r15%cpop r14%cpop r13%cpop r12%cmov rdi,0%ccall exit%cchild:%cmov rdi,cc%cmov rsi,argptr%cmov rdx,r13%cmov rax,59%csyscall%cjmp quit%c",0
+sh db "/bin/sh",0
+sharg db "-c",0
+cc db "/bin/nasm Sully5.s -felf64;/bin/clang Sully5.o -lc -o ./Sully",0
+filename db "Sully5.s",0
+exec db "./Sully",0
+argptr dq sh,sharg,cc,0
+exeptr dq exec,0
+s db "section .data%crecurs equ %d%csh db %c/bin/sh%c,0%csharg db %c-c%c,0%ccc db %c/bin/nasm Sully%d.s -felf64;/bin/clang Sully%d.o -lc -o ./Sully%c,0%cfilename db %cSully%d.s%c,0%cexec db %c./Sully%c,0%cargptr dq sh,sharg,cc,0%cexeptr dq exec,0%cs db %c%s%c,0%csection .text%cglobal main%cextern dprintf%cextern printf%cextern waitpid%cextern exit%c%c%%macro print 0%cpush rbp%cmov rbp,rsp%cmov rdi,r12%cmov rsi,s%cmov rdx,10%c;rcx is a 34%cmov r8,10%cmov r9,34%cmov rcx,116 ;change this value%cws:push 10%cloop ws%cpush 34%cpush s%cpush 34%cpush 10%cpush 10%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cpush r14%cpush 34%cpush 10%cpush 34%cpush r14%cpush r14%cpush 34%cpush 10%cpush 34%cpush 34%cpush 10%cpush 34%cmov rcx,r14%cxor rax,rax%ccall dprintf%cmov rcx,138%csa:pop rax%cloop sa%cpop rbp%c%%endmacro%c%cmain:%cxor rax,rax%ccmp rax,recurs%cjz	quit%cpush r12%cpush r13%cpush r14%cpush r15%cmov r14,recurs%csub r14,1%cmov r13,rdx%cmov rax,2%cmov rdi,filename%cmov rsi,01101o%cmov rdx,0640o%csyscall ;open%ccmp rax,0%cjs quit%cmov r12,rax%cprint%cmov rdi,r12%cmov rax,3%csyscall%cmov rax,57%csyscall ;fork%ccmp rax,0%cjs quit%ccmp rax,0%cje child%csub rsp,64%cmov rdi,rax%cmov rsi,rsp%cmov rdx,0%ccall waitpid%cadd rsp,64%cmov rdi,exec%cmov rsi,exeptr%cmov rdx,r13%cmov rax,59%csyscall 	;execve execution%cquit:%cpop r15%cpop r14%cpop r13%cpop r12%cmov rdi,0%ccall exit%cchild:%cmov rdi,sh%cmov rsi,argptr%cmov rdx,r13%cmov rax,59%csyscall%cjmp quit%c",0
 section .text
 global main
 extern dprintf
@@ -28,7 +24,7 @@ mov rdx,10
 ;rcx is a 34
 mov r8,10
 mov r9,34
-mov rcx,118 ;change this value
+mov rcx,116 ;change this value
 ws:push 10
 loop ws
 push 34
@@ -41,33 +37,22 @@ push 34
 push 34
 push 10
 push 34
-push 34
-push 10
-push 34
-push 34
-push 10
-push 34
 push r14
 push 34
 push 10
 push 34
-push 34
-push 10
-push 34
-push 34
-push 10
-push 34
-push 34
-push 10
-push 34
 push r14
+push r14
+push 34
+push 10
+push 34
 push 34
 push 10
 push 34
 mov rcx,r14
 xor rax,rax
 call dprintf
-mov rcx,151
+mov rcx,138
 sa:pop rax
 loop sa
 pop rbp
@@ -85,7 +70,7 @@ mov r14,recurs
 sub r14,1
 mov r13,rdx
 mov rax,2
-mov rdi,arg1
+mov rdi,filename
 mov rsi,01101o
 mov rdx,0640o
 syscall ;open
@@ -108,7 +93,7 @@ mov rsi,rsp
 mov rdx,0
 call waitpid
 add rsp,64
-mov rdi,arg8
+mov rdi,exec
 mov rsi,exeptr
 mov rdx,r13
 mov rax,59
@@ -121,7 +106,7 @@ pop r12
 mov rdi,0
 call exit
 child:
-mov rdi,cc
+mov rdi,sh
 mov rsi,argptr
 mov rdx,r13
 mov rax,59
